5775f51a67040af3e93342fbceb5f4336c2c7e48
raizenway <harisantoso0105@gmail.com>
2023-02-12 20:12:30 +0700

    Sedikit revisi

--- Praktikum4_Kasus2/src/ProdukMain.java
+++ Praktikum4_Kasus2/src/ProdukMain.java
@@ -3,25 +3,25 @@ public class ProdukMain {
 	public static void main(String[] args) {
 		Produk menu = new Produk ();
 		Penjualan pesan = new Penjualan(menu);
-		menu.tambahMenuMakanan("Bala-bala", 1_000, 14);
+		menu.tambahMenuMakanan("1. Bala-bala", 1_000, 14);
 		Produk.nextId();
-		menu.tambahMenuMakanan("Gehu", 1_000, 18);
+		menu.tambahMenuMakanan("2. Gehu", 1_000, 18);
 		Produk.nextId();
-		menu.tambahMenuMakanan("Tahu", 1_000, 17);
+		menu.tambahMenuMakanan("3. Tahu", 1_000, 17);
 		Produk.nextId();
-		menu.tambahMenuMakanan("Molen", 1_000, 20);
+		menu.tambahMenuMakanan("4. Molen", 1_000, 20);
 		Produk.nextId();
-		menu.tambahMenuMakanan("Pisang", 1_000, 16);
+		menu.tambahMenuMakanan("5. Pisang", 1_000, 16);
 		Produk.nextId();
-		menu.tambahMenuMakanan("Kroket", 1_000, 15);
+		menu.tambahMenuMakanan("6. Kroket", 1_000, 15);
 		Produk.nextId();
-		menu.tambahMenuMakanan("Cireng", 1_000, 19);
+		menu.tambahMenuMakanan("7. Cireng", 1_000, 19);
 		Produk.nextId();
-		menu.tambahMenuMakanan("Tempe", 1_000, 18);
+		menu.tambahMenuMakanan("8. Tempe", 1_000, 18);
 		Produk.nextId();
-		menu.tambahMenuMakanan("Bola Ubi", 1_000, 20);
+		menu.tambahMenuMakanan("9. Bola Ubi", 1_000, 20);
 		Produk.nextId();
-		menu.tambahMenuMakanan("Risol", 1_500, 10);
+		menu.tambahMenuMakanan("10. Risol", 1_500, 10);
 		Produk.nextId();
 
 		while(pesan.getKonfirm()==1) {
